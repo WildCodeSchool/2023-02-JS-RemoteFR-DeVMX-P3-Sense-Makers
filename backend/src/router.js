1043e99f0706a8a07aa532a102e547d264911eab
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-06-08 16:11:18 +0200

    Delete dead code in router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,10 +10,4 @@ router.put("/decisions/:id", decisionControllers.edit);
 router.post("/decisions", decisionControllers.add);
 router.delete("/decisions/:id", decisionControllers.destroy);
 
-// router.get("/items", itemControllers.browse);
-// router.get("/items/:id", itemControllers.read);
-// router.put("/items/:id", itemControllers.edit);
-// router.post("/items", itemControllers.add);
-// router.delete("/items/:id", itemControllers.destroy);
-
 module.exports = router;
