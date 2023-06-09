7d63d031fff82d37b27bb8aced18fb8a66378ffb
Frederico Cassola <cfcassola@gmail.com>
2023-06-09 06:28:35 +0200

    Add component header
    - Add scss _header file;
    - Add Component Header.jsx into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
 import "./scss/styles.scss";
 
 function App() {
-  return <div className="app" />;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
