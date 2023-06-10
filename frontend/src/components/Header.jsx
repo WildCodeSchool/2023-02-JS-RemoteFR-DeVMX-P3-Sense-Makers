201154e810530a67f90b539e45f01ced17c94228
Frederico Cassola <cfcassola@gmail.com>
2023-06-10 07:53:31 +0200

    Add icons folder and icons to login menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,10 +3,10 @@ import makeSenseLogo from "../assets/make_sense.png";
 import kikoAvatar from "../assets/avatar0.png";
 
 export default function Header() {
-  const [showHamburger, setShowHamburger] = useState(false);
+  const [showLoginMenu, setShowLoginMenu] = useState(false);
 
   const handleToggleBurger = () => {
-    setShowHamburger(!showHamburger);
+    setShowLoginMenu(!showLoginMenu);
   };
 
   return (
@@ -22,13 +22,82 @@ export default function Header() {
           onKeyDown={() => {}}
           onClick={() => handleToggleBurger()}
         >
-          {showHamburger && (
+          {showLoginMenu && (
             <nav className="login-menu">
               <ul>
-                <li>Parcourir les décisions</li>
-                <li>Mes décisions</li>
-                <li>Notifications</li>
-                <li>Administration</li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-grid"
+                  >
+                    <rect x="3" y="3" width="7" height="7" />
+                    <rect x="14" y="3" width="7" height="7" />
+                    <rect x="14" y="14" width="7" height="7" />
+                    <rect x="3" y="14" width="7" height="7" />
+                  </svg>
+                  Parcourir les décisions
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-home"
+                  >
+                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    <polyline points="9 22 9 12 15 12 15 22" />
+                  </svg>
+                  Mes décisions
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-bell"
+                  >
+                    <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+                    <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+                  </svg>
+                  Notifications
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-table"
+                  >
+                    <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" />
+                  </svg>
+                  Administration
+                </li>
               </ul>
               <ul>
                 <li>Mon compte</li>
