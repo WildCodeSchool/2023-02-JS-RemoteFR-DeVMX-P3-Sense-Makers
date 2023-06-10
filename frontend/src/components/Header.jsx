d7aba24e2266d1bed677fc65503e7ce44d64aaf6
Frederico Cassola <cfcassola@gmail.com>
2023-06-10 09:55:14 +0200

    Add css styling in login-menu on Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -43,7 +43,7 @@ export default function Header() {
                     <rect x="14" y="14" width="7" height="7" />
                     <rect x="3" y="14" width="7" height="7" />
                   </svg>
-                  Parcourir les décisions
+                  <div className="li-text">Parcourir les décisions</div>
                 </li>
                 <li>
                   <svg
@@ -61,7 +61,7 @@ export default function Header() {
                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
                     <polyline points="9 22 9 12 15 12 15 22" />
                   </svg>
-                  Mes décisions
+                  <div className="li-text">Mes décisions</div>
                 </li>
                 <li>
                   <svg
@@ -79,7 +79,7 @@ export default function Header() {
                     <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
                     <path d="M13.73 21a2 2 0 0 1-3.46 0" />
                   </svg>
-                  Notifications
+                  <div className="li-text">Notifications</div>
                 </li>
                 <li>
                   <svg
@@ -96,7 +96,7 @@ export default function Header() {
                   >
                     <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" />
                   </svg>
-                  Administration
+                  <div className="li-text">Administration</div>
                 </li>
               </ul>
               <ul>
