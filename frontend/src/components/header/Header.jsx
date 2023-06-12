02564e308f9aa0dcbe408b0e079e818dbc3798be
Frederico Cassola <cfcassola@gmail.com>
2023-06-12 17:02:03 +0200

    Clean comments

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -74,26 +74,6 @@ export default function Header() {
           </li>
           <li>
             <NotificationButton />
-            {/* <span
-              className={pathname === "*" ? "link-style active" : "link-style"}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-bell"
-              >
-                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-                <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-              </svg>
-              <div className="li-text">Notifications</div>
-            </span> */}
           </li>
           <li>
             <Link
