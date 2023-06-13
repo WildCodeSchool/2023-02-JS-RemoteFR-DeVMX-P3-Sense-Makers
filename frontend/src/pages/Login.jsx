0f543514c8bf34583fdfe99dc463101d127f15e5
ChaCharrier <charlotte.charrier97@gmail.com>
2023-06-13 17:11:26 +0200

    created label for the username in the form for the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,23 @@
+// import { useState } from "react";
+
 export default function Login() {
-  return <h1 className="login">login page</h1>;
+  // const [logInUsername, setLogInUsername] = useState("");
+  return (
+    <div className="logInContainer">
+      <form>
+        <div className="logIn-input">
+          <label htmlFor="logInUsername">Nom d'utilisateur</label>
+          <input
+          // autoComplete="nom d'utilisateur"
+          // id="logInUsername"
+          // type="text"
+          // onChange={(e) => {
+          //   setLogInUsername(e.target.value);
+          // }}
+          />
+          <label htmlFor="logInpassword">Mot de passe</label>
+        </div>
+      </form>
+    </div>
+  );
 }
