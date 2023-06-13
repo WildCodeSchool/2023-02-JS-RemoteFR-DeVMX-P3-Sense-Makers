795f0ae6caea9a07f29d305ef5a59f7b869a0fef
ChaCharrier <charlotte.charrier97@gmail.com>
2023-06-13 17:20:12 +0200

    created input for the password in the form for the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,20 +2,29 @@
 
 export default function Login() {
   // const [logInUsername, setLogInUsername] = useState("");
+  // const [logInPassword, setLogInPassword] = useState("");
   return (
     <div className="logInContainer">
       <form>
         <div className="logIn-input">
           <label htmlFor="logInUsername">Nom d'utilisateur</label>
           <input
-          // autoComplete="nom d'utilisateur"
-          // id="logInUsername"
-          // type="text"
-          // onChange={(e) => {
-          //   setLogInUsername(e.target.value);
-          // }}
+            autoComplete="nom d'utilisateur"
+            id="logInUsername"
+            type="text"
+            // onChange={(e) => {
+            //   setLogInUsername(e.target.value);
+            // }}
           />
-          <label htmlFor="logInpassword">Mot de passe</label>
+          <label htmlFor="logInPassword">Mot de passe</label>
+          <input
+            autoComplete="mot de passe"
+            id="logInPassword"
+            type="text"
+            // onChange={(e) => {
+            //   setLogInPassword(e.target.value);
+            // }}
+          />
         </div>
       </form>
     </div>
