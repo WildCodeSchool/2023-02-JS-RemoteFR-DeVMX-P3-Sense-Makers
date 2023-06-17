7158d3ec5dcc96d6cc06676f4c4e9db3e03bebca
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-06-17 12:37:30 +0200

    add usereducer and modification of home state for mapping and action button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
 import CardDecision from "../components/CardDecision";
 
 export default function Home() {
-  const [allDecisions, setAllDecision] = useState([]);
-  const [allStatus, setAllStatus] = useState([]);
-  const [currentStatus, setCurrentStatus] = useState();
+  const allStatesReducer = (state, action) => {
+    switch (action.type) {
+      case "allStates":
+        return {
+          ...state,
+          [action.key]: !action.value,
+        };
+      case "allStatus":
+        return {
+          ...state,
+          allStatus: action.value,
+        };
+      case "allDecisions":
+        return {
+          ...state,
+          allDecisions: action.value,
+        };
+      default:
+        return state;
+    }
+  };
 
-  const [showDecisions, setShowDecisions] = useState(false);
+  const initialStates = {
+    creation: false,
+    firstDecision: false,
+    conflict: false,
+    finalDecision: false,
+    unfinishedDecision: false,
+    decisionCompleted: false,
+    allStatus: [],
+    allDecisions: [],
+  };
+  const [allStates, dispatch] = useReducer(allStatesReducer, initialStates);
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/decisions`)
-      .then((res) => setAllDecision(res.data))
+      .then((res) => dispatch({ type: "allDecisions", value: res.data }))
       .catch((err) => {
         console.error(err);
       });
@@ -21,35 +49,37 @@ export default function Home() {
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/status`)
-      .then((res) => setAllStatus(res.data))
+      .then((res) => dispatch({ type: "allStatus", value: res.data }))
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
-  const ShowCurrentStatus = (statut) => {
-    if (statut.id === currentStatus) {
-      setShowDecisions(!showDecisions);
-    } else {
-      setCurrentStatus(statut.id);
-    }
-  };
-
   return (
     <div className="all-decisions-container">
       <h1>Toutes les décisions</h1>
-      {allStatus.map((statut) => (
+      {allStates.allStatus.map((statut, index) => (
         <div className="decisions-container" key={statut.id}>
           <div className="infos-status-container">
-            <button type="button" onClick={() => ShowCurrentStatus(statut)}>
+            <button
+              type="button"
+              onClick={() =>
+                dispatch({
+                  type: "allStates",
+                  value: allStates[Object.keys(allStates)[index]],
+                  key: Object.keys(initialStates)[index],
+                })
+              }
+            >
               <i className="fa-sharp fa-solid fa-caret-down" />
             </button>
             <h2>{statut.title}</h2>
           </div>
           <hr />
-          {showDecisions && statut.id === currentStatus && (
+
+          {allStates[[Object.keys(allStates)[index]]] && (
             <div className="cards-decision-container">
-              {allDecisions
+              {allStates.allDecisions
                 .filter((decision) => decision.title_status === statut.title)
                 .map((decision) => (
                   <CardDecision key={decision.id} decision={decision} />
