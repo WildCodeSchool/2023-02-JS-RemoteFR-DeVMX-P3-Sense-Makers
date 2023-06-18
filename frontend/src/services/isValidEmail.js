f8878609993436eb9afa0efb5eed94f23aa4ad2c
Frederico Cassola <cfcassola@gmail.com>
2023-06-18 16:39:59 +0200

    Add isValidEmail into services folder - still trying to validate photo

---
+++ frontend/src/services/isValidEmail.js
@@ -0,0 +1,7 @@
+export default function isValidEmail(email) {
+  const regex =
+    // eslint-disable-next-line no-useless-escape
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return regex.test(String(email).toLowerCase());
+}
