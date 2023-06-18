b639b7c99a3fd665600f38280bf6d99a52adf7dc
ChaCharrier <charlotte.charrier97@gmail.com>
2023-06-18 18:21:44 +0200

    created component for graphic elements of the login page + started creating shapes for mobile version of login page

---
+++ frontend/src/components/graphicElements/GraphicElements.jsx
@@ -0,0 +1,18 @@
+export default function graphicElements() {
+  return (
+    <div className="bottom-left-deco">
+      <div className="rectangle1" />
+      <div className="circle1" />
+      <div className="rectangle2" />
+      <div className="circle2" />
+      <div className="rectangle3" />
+      <div className="circle3" />
+      <div className="rectangle4" />
+      <div className="circle4" />
+      <div className="rectangle5" />
+      <div className="circle5" />
+      <div className="rectangle6" />
+      <div className="circle6" />
+    </div>
+  );
+}
