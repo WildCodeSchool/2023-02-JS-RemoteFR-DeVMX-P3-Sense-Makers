20edefb20d0314a8c6c1b04f88b010f3efd8a2f3
Alaric HENROT <alaric.henrot@gmail.com>
2023-06-19 15:36:34 +0200

    change decision.jsx for css and add css

--- frontend/src/pages/Decision.jsx
+++ frontend/src/pages/Decision.jsx
@@ -85,121 +85,144 @@ export default function Decision() {
 
   return (
     <div className="decision">
-      <div className="status-hub">
-        <div className="status">{decision.title_status}</div>
-        <div className="hub">{decision.concerned_hub}</div>
-      </div>
-      <h1>{decision.title_decision}</h1>
-      <div className="author">
-        <img
-          src={decision.photo}
-          alt={`${decision.firstname} ${decision.lastname}`}
-        />
-        <p>
-          par{" "}
-          <span className="bold-text">
-            {decision.firstname} {decision.lastname}
-          </span>
-        </p>
-      </div>
-      <div className="sections">
-        <button type="button" onClick={() => dispatch({ type: "details" })}>
-          <i className="fa-sharp fa-solid fa-caret-down" />
-        </button>
-        <h2>Les details de la décison</h2>
-      </div>
-      <hr />
-      {showCategories.details && (
-        <div>
-          {decision.context}
-          {decision.content}{" "}
+      <div className="main-content">
+        <div className="status-hub">
+          <div className="status">{decision.title_status}</div>
+          <div className="hub">{decision.concerned_hub}</div>
         </div>
-      )}
-      <div className="sections">
-        <button type="button" onClick={() => dispatch({ type: "impact" })}>
-          <i className="fa-sharp fa-solid fa-caret-down" />
-        </button>
-        <h2>Impact sur l'organisation</h2>
-      </div>
-      <hr />
-      {showCategories.impact && <div>{decision.usefulness}</div>}
-      <div className="sections">
-        <button type="button" onClick={() => dispatch({ type: "benefits" })}>
-          <i className="fa-sharp fa-solid fa-caret-down" />
-        </button>
-        <h2>Bénéfices</h2>
-      </div>
-      <hr />
-      {showCategories.benefits && <div>{decision.benefit}</div>}
-      <div className="sections">
-        <button
-          type="button"
-          onClick={() => dispatch({ type: "disadvantages" })}
-        >
-          <i className="fa-sharp fa-solid fa-caret-down" />
-        </button>
-        <h2>Risques potentiels</h2>
-      </div>
-      <hr />
-      {showCategories.disadvantages && <div>{decision.disavantages}</div>}
-      <div className="sections">
-        <button type="button" onClick={() => dispatch({ type: "comments" })}>
-          <i className="fa-sharp fa-solid fa-caret-down" />
-        </button>
-        <h2>Avis</h2>
-      </div>
-      <hr />
-      {showCategories.comments && (
-        <div className="comments">
-          {comments.map((comment) => (
-            <div key={comment.id}>
-              <div>
-                {" "}
-                <img
-                  src={comment.photo}
-                  alt={`${comment.firstname} ${comment.lastname}`}
-                />{" "}
-                <div className="bold-text">
-                  {comment.firstname} {comment.lastname}{" "}
-                  {experts.some((expert) => expert.id === comment.user_id) ? (
-                    <p>expert</p>
-                  ) : (
-                    impactedUsers.some(
-                      (impactedUser) => impactedUser.id === comment.user_id
-                    ) && <p>impacté par la décision</p>
-                  )}{" "}
+        <h1>{decision.title_decision}</h1>
+        <div className="author">
+          <img
+            src={decision.photo}
+            alt={`${decision.firstname} ${decision.lastname}`}
+          />
+          <p>
+            par{" "}
+            <span className="bold-text">
+              {decision.firstname} {decision.lastname}
+            </span>
+          </p>
+        </div>
+        <div className="sections">
+          <button type="button" onClick={() => dispatch({ type: "details" })}>
+            <i className="fa-sharp fa-solid fa-caret-down" />
+          </button>
+          <h2 className={showCategories.details && "opened-section"}>
+            Les details de la décison
+          </h2>
+        </div>
+        <hr />
+        {showCategories.details && (
+          <div className="sections-content">
+            <p>{decision.context}</p>
+            <p>{decision.content}</p>
+          </div>
+        )}
+        <div className="sections">
+          <button type="button" onClick={() => dispatch({ type: "impact" })}>
+            <i className="fa-sharp fa-solid fa-caret-down" />
+          </button>
+          <h2 className={showCategories.impact && "opened-section"}>
+            Impact sur l'organisation
+          </h2>
+        </div>
+        <hr />
+        {showCategories.impact && (
+          <div className="sections-content">{decision.usefulness}</div>
+        )}
+        <div className="sections">
+          <button type="button" onClick={() => dispatch({ type: "benefits" })}>
+            <i className="fa-sharp fa-solid fa-caret-down" />
+          </button>
+          <h2 className={showCategories.benefits && "opened-section"}>
+            Bénéfices
+          </h2>
+        </div>
+        <hr />
+        {showCategories.benefits && (
+          <div className="sections-content">{decision.benefit}</div>
+        )}
+        <div className="sections">
+          <button
+            type="button"
+            onClick={() => dispatch({ type: "disadvantages" })}
+          >
+            <i className="fa-sharp fa-solid fa-caret-down" />
+          </button>
+          <h2 className={showCategories.disadvantages && "opened-section"}>
+            Risques potentiels
+          </h2>
+        </div>
+        <hr />
+        {showCategories.disadvantages && (
+          <div className="sections-content">{decision.disavantages}</div>
+        )}
+        <div className="sections">
+          <button type="button" onClick={() => dispatch({ type: "comments" })}>
+            <i className="fa-sharp fa-solid fa-caret-down" />
+          </button>
+          <h2 className={showCategories.comments && "opened-section"}>Avis</h2>
+        </div>
+        <hr />
+        {showCategories.comments && (
+          <div className="comments sections-content">
+            {comments.map((comment) => (
+              <div key={comment.id}>
+                <div>
+                  {" "}
+                  <img
+                    src={comment.photo}
+                    alt={`${comment.firstname} ${comment.lastname}`}
+                  />{" "}
+                  <div className="bold-text">
+                    {comment.firstname} {comment.lastname}{" "}
+                    {experts.some((expert) => expert.id === comment.user_id) ? (
+                      <p>expert</p>
+                    ) : (
+                      impactedUsers.some(
+                        (impactedUser) => impactedUser.id === comment.user_id
+                      ) && <p>impacté par la décision</p>
+                    )}{" "}
+                  </div>
                 </div>
+                {comment.comment}
               </div>
-              {comment.comment}
+            ))}
+          </div>
+        )}
+        <button type="button" className="comment-button">
+          Donner mon avis
+        </button>
+      </div>
+      <div className="side-content">
+        <div>
+          <h2>Dates à retenir</h2>
+          <div> timeline</div>
+          <h2>Personnes impactées</h2>
+          {impactedUsers.map((impactedUser) => (
+            <div key={impactedUser.id}>
+              <img
+                src={impactedUser.photo}
+                alt={`${impactedUser.firstname} ${impactedUser.lastname}`}
+              />{" "}
+              {impactedUser.firstname} {impactedUser.lastname}{" "}
+            </div>
+          ))}
+          <h2>Personnes expertes</h2>
+          {experts.map((expert) => (
+            <div key={expert.id}>
+              <img
+                src={expert.photo}
+                alt={`${expert.firstname} ${expert.lastname}`}
+              />{" "}
+              {expert.photo} {expert.firstname} {expert.lastname}{" "}
             </div>
           ))}
         </div>
-      )}
-      <button type="button" className="comment-button">
-        Donner mon avis
-      </button>
-      <h2>Dates à retenir</h2>
-      <div> timeline</div>
-      <h2>Personnes impactées</h2>
-      {impactedUsers.map((impactedUser) => (
-        <div key={impactedUser.id}>
-          <img
-            src={impactedUser.photo}
-            alt={`${impactedUser.firstname} ${impactedUser.lastname}`}
-          />{" "}
-          {impactedUser.firstname} {impactedUser.lastname}{" "}
-        </div>
-      ))}
-      <h2>Personnes expertes</h2>
-      {experts.map((expert) => (
-        <div key={expert.id}>
-          <img src={expert.photo} alt={`${expert.photo} ${expert.firstname}`} />{" "}
-          {expert.photo} {expert.firstname} {expert.lastname}{" "}
-        </div>
-      ))}
-      <button type="button" className="comment-button">
-        Donner mon avis
-      </button>
+        <button type="button" className="comment-button">
+          Donner mon avis
+        </button>
+      </div>
     </div>
   );
 }
