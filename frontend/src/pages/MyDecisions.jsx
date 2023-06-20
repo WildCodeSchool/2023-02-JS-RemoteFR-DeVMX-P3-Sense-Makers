e00a24149a7bee6d94e4a8d2d354f96518e85faa
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-06-20 17:06:54 +0200

    deleted useReducer and add details balise , update style css

--- frontend/src/pages/MyDecisions.jsx
+++ frontend/src/pages/MyDecisions.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useReducer } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import CardDecision from "../components/CardDecision";
@@ -8,28 +8,6 @@ export default function MyDecisions() {
   const { id } = useParams();
   const [allDecisions, setAllDecision] = useState([]);
   const [allStatus, setAllStatus] = useState([]);
-
-  const allStatesReducer = (state, action) => {
-    switch (action.type) {
-      case "allStates":
-        return {
-          ...state,
-          [action.key]: !action.value,
-        };
-      default:
-        return state;
-    }
-  };
-
-  const initialStates = {
-    creation: false,
-    firstDecision: false,
-    conflict: false,
-    finalDecision: false,
-    unfinishedDecision: false,
-    decisionCompleted: false,
-  };
-  const [allStates, dispatch] = useReducer(allStatesReducer, initialStates);
 
   useEffect(() => {
     axios
@@ -58,25 +36,13 @@ export default function MyDecisions() {
           Créer une décision
         </button>
       </div>
-      {allStatus.map((statut, index) => (
-        <div className="decisions-container" key={statut.id}>
-          <div className="infos-status-container">
-            <button
-              type="button"
-              onClick={() =>
-                dispatch({
-                  type: "allStates",
-                  value: allStates[Object.keys(allStates)[index]],
-                  key: Object.keys(initialStates)[index],
-                })
-              }
-            >
-              <i className="fa-sharp fa-solid fa-caret-down" />
-            </button>
-            <h2>{statut.title}</h2>
-          </div>
-          <hr />
-          {allStates[[Object.keys(allStates)[index]]] && (
+      {allStatus.map((statut) => (
+        <div className="infos-status-container" key={statut.id}>
+          <details className="details-container">
+            <summary>
+              {statut.title}
+              <hr />
+            </summary>
             <div className="cards-decision-container">
               {allDecisions
                 .filter((decision) => decision.title_status === statut.title)
@@ -84,7 +50,7 @@ export default function MyDecisions() {
                   <CardDecision key={decision.d_id} decision={decision} />
                 ))}
             </div>
-          )}
+          </details>
         </div>
       ))}
     </div>
