0e7e61f68e7e11689c3bd24e6c5632d0a45a8228
Alaric HENROT <alaric.henrot@gmail.com>
2023-06-21 12:04:51 +0200

    change queries to match front for decision, change date in db, add uuid in uploadFile.js

--- backend/src/models/commentsManager.js
+++ backend/src/models/commentsManager.js
@@ -7,7 +7,7 @@ class CommentsManager extends AbstractManager {
 
   findAllCommentsForOneDecision(decisionId) {
     return this.database.query(
-      `select c.user_id, c.id, c.comment, c.vote, c.creation_date, u.firstname, u.lastname, u.photo from ${this.table} c
+      `select c.user_id, c.id, c.comment, c.vote, DATE_FORMAT(c.creation_date, "%d/%m/%Y") AS date , u.firstname, u.lastname, u.photo from ${this.table} c
       INNER JOIN users u ON c.user_id = u.id
       where decision_id = ?`,
       [decisionId]
@@ -16,7 +16,7 @@ class CommentsManager extends AbstractManager {
 
   insert(comment, decisionId) {
     return this.database.query(
-      `insert into ${this.table} (user_id, decision_id, creation_date, comment, vote) values (?,?,?,?,?)`,
+      `insert into ${this.table} (user_id, decision_id, comment, vote) values (?,?,?,?,?)`,
       [
         comment.user_id,
         decisionId,
@@ -24,13 +24,6 @@ class CommentsManager extends AbstractManager {
         comment.comment,
         comment.vote,
       ]
-    );
-  }
-
-  update(comment) {
-    return this.database.query(
-      `update ${this.table} set comment = ?, vote = ? where id = ?`,
-      [comment.comment, comment.vote, comment.commentid]
     );
   }
 }
