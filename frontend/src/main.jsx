84f65fa876efe5d7fd5370b4f8ceba7cd47d0271
ChaCharrier <charlotte.charrier97@gmail.com>
2023-07-05 16:42:34 +0200

    created userContext, get user from login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { UserProvider } from "./contexts/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
