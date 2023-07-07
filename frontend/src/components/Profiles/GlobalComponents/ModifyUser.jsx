895ac9e8d20c25432ef7a483dcc9a322ba888431
Frederico Cassola <cfcassola@gmail.com>
2023-07-07 06:59:52 +0200

    Add close-modal button html and css

--- frontend/src/components/Profiles/GlobalComponents/ModifyUser.jsx
+++ frontend/src/components/Profiles/GlobalComponents/ModifyUser.jsx
@@ -115,6 +115,25 @@ export default function ModifyUser() {
     <form className="modify-user-management" onSubmit={submit}>
       <div className="add-user-title-container">
         <h2 className="add-user-title">Modification d'utilisateur</h2>
+        <div className="close-modal-button-container">
+          <button type="button" className="close-modal-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-x"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </button>
+        </div>
         <div className="remove-button-container-2">
           <button type="button">Supprimer</button>
         </div>
