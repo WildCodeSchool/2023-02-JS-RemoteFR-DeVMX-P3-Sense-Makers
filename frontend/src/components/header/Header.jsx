dd0a908de1b0a445a295e9b12ace0dff44722744
Frederico Cassola <cfcassola@gmail.com>
2023-07-10 11:11:44 +0200

    Add administration conditional by user role on menu and nav

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import LoginButton from "./ProfileMenuButton";
 import NotificationButton from "./NotificationsMenu";
 import makeSenseLogo from "../../assets/make_sense.png";
+import userContext from "../../contexts/userContext";
 
 export default function Header() {
   const [showLoginMenu, setShowLoginMenu] = useState(false);
   const [showNotificationsMenu, setShowNotificationsMenu] = useState(false);
 
   const { pathname } = useLocation();
+  const { user } = useContext(userContext);
 
   const handleShowNotificationsMenu = () => {
     setShowNotificationsMenu(!showNotificationsMenu);
@@ -115,32 +117,34 @@ export default function Header() {
               />
             )}
           </li>
-          <li>
-            <Link
-              className={
-                pathname === "/logged/profile"
-                  ? "link-style active"
-                  : "link-style"
-              }
-              to="/logged/profile"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-table"
+          {user.role_id === 1 && (
+            <li>
+              <Link
+                className={
+                  pathname === "/logged/profile"
+                    ? "link-style active"
+                    : "link-style"
+                }
+                to="/logged/profile"
               >
-                <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" />
-              </svg>
-              <div className="li-text">Administration</div>
-            </Link>
-          </li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-table"
+                >
+                  <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" />
+                </svg>
+                <div className="li-text">Administration</div>
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
       <LoginButton
@@ -150,6 +154,7 @@ export default function Header() {
         showNotificationsMenu={showNotificationsMenu}
         setShowNotificationsMenu={setShowNotificationsMenu}
         handleShowNotificationsMenu={handleShowNotificationsMenu}
+        userRoleId={user.role_id}
       />
     </div>
   );
