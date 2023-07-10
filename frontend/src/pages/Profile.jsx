d223c824bd3082a5c9627427fcfd89ebc786cc73
Frederico Cassola <cfcassola@gmail.com>
2023-07-10 12:16:11 +0200

    Add reinicialize password ok in modifyUser

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,10 @@
 import AdminProfile from "../components/Profiles/AdminProfile";
 import UserProfile from "../components/Profiles/UserProfile";
-import userContext from "../contexts/userContext";
 
 export default function Profile() {
-  const { user } = userContext(userContext);
   return (
     <div className="profile">
-      {user.role_id === 1 ? <AdminProfile /> : <UserProfile />}
+      <AdminProfile /> <UserProfile />
     </div>
   );
 }
