b05ede921d9f54e83f3c382cf8fc180cf53a1a29
Frederico Cassola <cfcassola@gmail.com>
2023-07-10 11:25:29 +0200

    Trying the conditionl admin and usr profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,12 @@
 import AdminProfile from "../components/Profiles/AdminProfile";
 import UserProfile from "../components/Profiles/UserProfile";
+import userContext from "../contexts/userContext";
 
 export default function Profile() {
+  const { user } = userContext(userContext);
   return (
     <div className="profile">
-      <AdminProfile />
-      <UserProfile />
+      {user.role_id === 1 ? <AdminProfile /> : <UserProfile />}
     </div>
   );
 }
