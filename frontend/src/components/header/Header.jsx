a00281045fdeafc8d89146df94c15fd4fe3c500c
Alaric HENROT <alaric.henrot@gmail.com>
2023-07-12 12:04:18 +0200

    add translation logic and use it in header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import LoginButton from "./ProfileMenuButton";
 import NotificationButton from "./NotificationsMenu";
 import makeSenseLogo from "../../assets/make_sense.png";
 import userContext from "../../contexts/userContext";
+import Lang from "../Lang";
 
 export default function Header() {
   const [showLoginMenu, setShowLoginMenu] = useState(false);
@@ -11,6 +13,7 @@ export default function Header() {
 
   const { pathname } = useLocation();
   const { user } = useContext(userContext);
+  const { t } = useTranslation();
 
   const handleShowNotificationsMenu = () => {
     setShowNotificationsMenu(!showNotificationsMenu);
@@ -26,6 +29,9 @@ export default function Header() {
       </div>
       <nav className="navBar-icons">
         <ul className="navBar-list">
+          <li>
+            <Lang />
+          </li>
           <li>
             <Link
               className={
@@ -50,7 +56,7 @@ export default function Header() {
                 <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
                 <polyline points="9 22 9 12 15 12 15 22" />
               </svg>
-              <div className="li-text">Décisions</div>
+              <div className="li-text">{t("header.decision")}</div>
             </Link>
           </li>
           <li>
@@ -80,7 +86,7 @@ export default function Header() {
                 <rect x="3" y="14" width="7" height="7" />
               </svg>
 
-              <div className="li-text">Mes décisions</div>
+              <div className="li-text">{t("header.my-decisions")}</div>
             </Link>
           </li>
           <li>
@@ -106,7 +112,7 @@ export default function Header() {
                 <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
                 <path d="M13.73 21a2 2 0 0 1-3.46 0" />
               </svg>
-              <div className="notification-icon-title">Notifications</div>
+              <div className="notification-icon-title">{t("header.notif")}</div>
             </div>
 
             {showNotificationsMenu && (
@@ -141,7 +147,7 @@ export default function Header() {
                 >
                   <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" />
                 </svg>
-                <div className="li-text">Administration</div>
+                <div className="li-text">{t("header.admin")}</div>
               </Link>
             </li>
           )}
---
+++ frontend/src/services/Languages.ts
@@ -0,0 +1,5 @@
+export enum Languages {
+  FR = "fr",
+  EN = "en",
+  ES = "es",
+}
