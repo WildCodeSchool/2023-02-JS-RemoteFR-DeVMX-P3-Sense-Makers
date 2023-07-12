c206c91c4032d2d4aa62fd948a228a6df3fd8eb5
Frederico Cassola <cfcassola@gmail.com>
2023-07-12 15:40:01 +0200

    Remove button to close component

--- frontend/src/components/Profiles/GlobalComponents/ModifyMyProfil.jsx
+++ frontend/src/components/Profiles/GlobalComponents/ModifyMyProfil.jsx
@@ -74,25 +74,6 @@ export default function ModifyMyProfil() {
     <form className="modify-my-profil-management" onSubmit={submit}>
       <div className="add-user-title-container">
         <h2 className="add-user-title">Modification d'utilisateur</h2>
-        <div className="close-modal-button-container">
-          <button type="button" className="close-modal-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-x"
-            >
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
-          </button>
-        </div>
       </div>
       <div className="user-management-container">
         <div className="input-container">
