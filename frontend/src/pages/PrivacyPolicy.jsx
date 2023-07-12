1a2600531382175ce980694ebd7bbc2d5709b9a1
ChaCharrier <charlotte.charrier97@gmail.com>
2023-07-12 18:30:59 +0200

    ajusted css for privacyPolicy's modal and created accept and decline buttons at the bottom

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -1,98 +1,135 @@
+import PropTypes from "prop-types";
 import GraphicElements from "../components/graphicElements/GraphicElements";
 
-export default function PrivacyPolicies() {
+function PrivacyPolicy({ setShowCookieBanner, setCookieValidation }) {
+  const handleClickAccept = () => {
+    localStorage.setItem("conditionsAcceptation", "true");
+    setShowCookieBanner(false);
+    setCookieValidation("hide");
+  };
+
+  const handleClickRefuse = () => {
+    localStorage.setItem("conditionsRefused", "false");
+    setShowCookieBanner(true);
+  };
+
   return (
     <>
       <GraphicElements />
       <div className="privacy-policy-container">
-        <h1>POLITIQUE DE CONFIDENTIALITÉ</h1>
-        <h2>DONNÉES PERSONNELLES</h2>
-        <div>
-          <h3>DÉFINITIONS :</h3>
-          <p>
-            <strong>L'Éditeur</strong> : La personne, physique ou morale, qui
-            édite les services de communication au public en ligne.{" "}
-          </p>
-          <p>
-            <strong>Le Site</strong> : L'ensemble des sites, pages Internet et
-            services en ligne proposés par l'Éditeur.{" "}
-          </p>
-          <p>
-            <strong>L'Utilisateur</strong> : La personne utilisant le Site et
-            les services.
-          </p>
-        </div>
-        <div>
-          <h3>1 - COLLECTE DES DONNÉES D'IDENTITÉ :</h3>
-          <p>
-            <strong>
-              Inscription et identification préalable pour la fourniture du
-              service
-            </strong>
-          </p>
-          <p>
-            L’utilisation du Site nécessite une inscription et une
-            identification préalable. Vos données nominatives (nom, prénom,
-            e-mail,...) sont utilisées pour exécuter nos obligations légales
-            résultant de la livraison des produits et / ou des services, en
-            vertu du Contrat de licence utilisateur final, de la Limite de
-            garantie, le cas échéant, ou de toute autre condition applicable.
-            Vous ne fournirez pas de fausses informations nominatives et ne
-            créerez pas de compte pour une autre personne sans son autorisation.
-            Vos coordonnées devront toujours être exactes et à jour.
-          </p>
-        </div>
-        <div>
-          <h3>2 - COLLECTE DES DONNÉES D’IDENTIFICATION : </h3>
-          <p>
-            <strong>
-              Utilisation de l'identifiant de l’utilisateur uniquement pour
-              l’accès aux services.
-            </strong>
-          </p>
-          <p>
-            Nous utilisons vos identifiants électroniques seulement pour et
-            pendant l'exécution du contrat.
-          </p>
-        </div>
-        <div>
-          <h3>3 - COLLECTE DES DONNÉES DU TERMINAL : </h3>
-          <p>
-            <strong>Aucune collecte des données techniques</strong>
-          </p>
-          <p>
-            Nous ne collectons et ne conservons aucune donnée technique de votre
-            appareil (adresse IP, fournisseur d'accès à Internet...).
-          </p>
-        </div>
-        <div>
-          <h3>4 - COOKIES : </h3>
-          <p>
-            <strong>
-              Droit de l'Utilisateur de refuser les cookies, la désactivation
-              entraînant un fonctionnement dégradé du service{" "}
-            </strong>
-          </p>
-          <p>
-            Vous reconnaissez avoir été informé que l'Éditeur peut avoir recours
-            à des cookies, et l'y autorisez. Si vous ne souhaitez pas que des
-            cookies soient utilisés sur votre terminal, la plupart des
-            navigateurs vous permettent de désactiver les cookies en passant par
-            les options de réglage. Toutefois, vous êtes informé que certains
-            services sont susceptibles de ne plus fonctionner correctement.{" "}
-          </p>
-          <p>
-            <strong>
-              Association possible des cookies avec des données personnelles
-              pour permettre le fonctionnement du service{" "}
-            </strong>
-          </p>
-          <p>
-            L'Éditeur peut être amenée à recueillir des informations de
-            navigation via l'utilisation de cookies.
-          </p>
+        <div className="text-container">
+          <h1>POLITIQUE DE CONFIDENTIALITÉ</h1>
+          <div>
+            <h3>DÉFINITIONS :</h3>
+            <p>
+              <strong>L'Éditeur</strong> : La personne, physique ou morale, qui
+              édite les services de communication au public en ligne.{" "}
+            </p>
+            <p>
+              <strong>Le Site</strong> : L'ensemble des sites, pages Internet et
+              services en ligne proposés par l'Éditeur.{" "}
+            </p>
+            <p>
+              <strong>L'Utilisateur</strong> : La personne utilisant le Site et
+              les services.
+            </p>
+          </div>
+          <div>
+            <h3>1 - COLLECTE DES DONNÉES D'IDENTITÉ :</h3>
+            <p>
+              <strong>
+                Inscription et identification préalable pour la fourniture du
+                service
+              </strong>
+            </p>
+            <p>
+              L’utilisation du Site nécessite une inscription et une
+              identification préalable. Vos données nominatives (nom, prénom,
+              e-mail,...) sont utilisées pour exécuter nos obligations légales
+              résultant de la livraison des produits et / ou des services, en
+              vertu du Contrat de licence utilisateur final, de la Limite de
+              garantie, le cas échéant, ou de toute autre condition applicable.
+              Vous ne fournirez pas de fausses informations nominatives et ne
+              créerez pas de compte pour une autre personne sans son
+              autorisation. Vos coordonnées devront toujours être exactes et à
+              jour.
+            </p>
+          </div>
+          <div>
+            <h3>2 - COLLECTE DES DONNÉES D’IDENTIFICATION : </h3>
+            <p>
+              <strong>
+                Utilisation de l'identifiant de l’utilisateur uniquement pour
+                l’accès aux services.
+              </strong>
+            </p>
+            <p>
+              Nous utilisons vos identifiants électroniques seulement pour et
+              pendant l'exécution du contrat.
+            </p>
+          </div>
+          <div>
+            <h3>3 - COLLECTE DES DONNÉES DU TERMINAL : </h3>
+            <p>
+              <strong>Aucune collecte des données techniques</strong>
+            </p>
+            <p>
+              Nous ne collectons et ne conservons aucune donnée technique de
+              votre appareil (adresse IP, fournisseur d'accès à Internet...).
+            </p>
+          </div>
+          <div>
+            <h3>4 - COOKIES : </h3>
+            <p>
+              <strong>
+                Droit de l'Utilisateur de refuser les cookies, la désactivation
+                entraînant un fonctionnement dégradé du service{" "}
+              </strong>
+            </p>
+            <p>
+              Vous reconnaissez avoir été informé que l'Éditeur peut avoir
+              recours à des cookies, et l'y autorisez. Si vous ne souhaitez pas
+              que des cookies soient utilisés sur votre terminal, la plupart des
+              navigateurs vous permettent de désactiver les cookies en passant
+              par les options de réglage. Toutefois, vous êtes informé que
+              certains services sont susceptibles de ne plus fonctionner
+              correctement.{" "}
+            </p>
+            <p>
+              <strong>
+                Association possible des cookies avec des données personnelles
+                pour permettre le fonctionnement du service{" "}
+              </strong>
+            </p>
+            <p>
+              L'Éditeur peut être amenée à recueillir des informations de
+              navigation via l'utilisation de cookies.
+            </p>
+          </div>
+          <div className="container-button">
+            <button
+              type="button"
+              className="handle-cookie-btn firstBtn"
+              onClick={handleClickAccept}
+            >
+              Accord
+            </button>
+            <button
+              type="button"
+              className="handle-cookie-btn"
+              onClick={handleClickRefuse}
+            >
+              Refus
+            </button>
+          </div>
         </div>
       </div>
     </>
   );
 }
+PrivacyPolicy.propTypes = {
+  setShowCookieBanner: PropTypes.func.isRequired,
+  setCookieValidation: PropTypes.func.isRequired,
+};
+
+export default PrivacyPolicy;
