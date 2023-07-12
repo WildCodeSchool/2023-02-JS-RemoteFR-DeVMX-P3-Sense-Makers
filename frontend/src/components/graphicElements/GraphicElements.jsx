570b8b1a871e54c06bec8c44e117e0da9385d290
ChaCharrier <charlotte.charrier97@gmail.com>
2023-07-12 12:36:18 +0200

    deleted useless code

--- frontend/src/components/graphicElements/GraphicElements.jsx
+++ frontend/src/components/graphicElements/GraphicElements.jsx
@@ -34,38 +34,6 @@ export default function graphicElements() {
           alt="bottom right decoration"
         />
       </div>
-      {/* <div className="bottom-left-deco">
-        <div className="rectangle1" />
-        <div className="circle1" />
-        <div className="rectangle2" />
-        <div className="circle2" />
-        <div className="rectangle3" />
-        <div className="circle3" />
-        <div className="rectangle4" />
-        <div className="circle4" />
-        <div className="rectangle5" />
-        <div className="circle5" />
-        <div className="rectangle6" />
-        <div className="circle6" />
-      </div>
-      <div className="top-left-deco">
-        <div className="pill-border">
-          <div className="rectangle-pill-border" />
-          <div className="circle-pill-border" />
-        </div>
-        <div className="little-pill" />
-      </div>
-      <div className="top-right-deco">
-        <div className="small-green-rectangle" />
-        <div className="small-green-circle1" />
-        <div className="small-green-circle2" />
-        <div className="small-red-rectangle" />
-        <div className="small-red-circle1" />
-        <div className="small-red-circle2" />
-        <div className="small-yellow-rectangle" />
-        <div className="small-yellow-circle1" />
-        <div className="small-yellow-circle2" />
-      </div> */}
     </>
   );
 }
