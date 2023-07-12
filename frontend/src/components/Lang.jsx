607de945670a34611399ee4a144d5097a4b772f0
Alaric HENROT <alaric.henrot@gmail.com>
2023-07-12 20:00:24 +0200

    remove state in lang for now + linter formating scss

--- frontend/src/components/Lang.jsx
+++ frontend/src/components/Lang.jsx
@@ -1,26 +1,21 @@
-import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Languages } from "../services/Languages.ts";
 
 export default function Lang() {
   const { i18n } = useTranslation();
-  const [lang, setLang] = useState(i18n.languages);
 
   const changeLanguage = (event) => {
     const languages = event.target.value;
 
     switch (languages) {
       case Languages.EN:
-        setLang(Languages.EN);
         i18n.changeLanguage(Languages.EN);
         break;
       case Languages.ES:
-        setLang(Languages.ES);
         i18n.changeLanguage(Languages.ES);
         break;
       case Languages.FR:
       default:
-        setLang(Languages.FR);
         i18n.changeLanguage(Languages.FR);
         break;
     }
@@ -29,7 +24,7 @@ export default function Lang() {
   return (
     <div>
       <div>
-        <select value={lang} name="languages" onChange={changeLanguage}>
+        <select name="languages" onChange={changeLanguage}>
           <option value={Languages.FR}>FR</option>
           <option value={Languages.EN}>EN</option>
           <option value={Languages.ES}>ES</option>
