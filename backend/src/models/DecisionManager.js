aeb6400a31783c28e748d68302cff2769acc0c5c
Damien Arnaud <113262967+Zanelidd@users.noreply.github.com>
2023-07-13 17:45:49 +0200

    change in decision manager et database for notification

--- backend/src/models/DecisionManager.js
+++ backend/src/models/DecisionManager.js
@@ -63,7 +63,7 @@ class DecisionManager extends AbstractManager {
 
   findImpactedOnDecisionById(id) {
     return this.database.query(
-      `SELECT u.id, u.firstname, u.lastname, u.photo FROM tagged_as_impacted ti
+      `SELECT u.id, u.firstname, u.lastname, u.photo, te.is_notif_read FROM tagged_as_impacted ti
       INNER JOIN users u ON u.id = ti.user_id
     where ti.decision_id = ?`,
       [id]
@@ -72,7 +72,7 @@ class DecisionManager extends AbstractManager {
 
   findExpertOnDecisionById(id) {
     return this.database.query(
-      `SELECT u.id, u.firstname, u.lastname, u.photo  FROM tagged_as_experts te
+      `SELECT u.id, u.firstname, u.lastname, u.photo, te.is_notif_read  FROM tagged_as_experts te
       INNER JOIN users u ON u.id = te.user_id
     where te.decision_id = ?`,
       [id]
@@ -101,7 +101,7 @@ class DecisionManager extends AbstractManager {
   }
 
   findConcernedHub() {
-    return this.database.query(`SELECT * FROM concernedhub c`);
+    return this.database.query(`SELECT * FROM concernedhub`);
   }
 }
 
