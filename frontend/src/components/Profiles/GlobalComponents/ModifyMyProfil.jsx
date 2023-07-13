efd7c88372e63022722376322e3c4f6e115cf35e
Frederico Cassola <cfcassola@gmail.com>
2023-07-13 08:13:29 +0200

    Clean CSS

--- frontend/src/components/Profiles/GlobalComponents/ModifyMyProfil.jsx
+++ frontend/src/components/Profiles/GlobalComponents/ModifyMyProfil.jsx
@@ -139,7 +139,6 @@ export default function ModifyMyProfil() {
             />
           </label>
         </div>
-
         <div className="input-buttons-container">
           <div className="roles-container-2">
             <div className="role-actuel">
